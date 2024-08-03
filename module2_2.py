3dba0d5e25f74c2b229553a17a3d282ecdc75906
Demid847 <kriper847@gmail.com>
2024-08-03 11:42:55 +0300

    Update module2_2.py

--- module2_2.py
+++ module2_2.py
@@ -1,7 +1,10 @@
 MyList = [42,69,322,13,0,99,-5,9,8,7,-6,5]
 a = 0
 while a < len(MyList) :
-    if MyList[a] > 0:
+    if MyList[a] == 0:
+        a += 1
+        continue
+    elif MyList[a] >= 0:
         print(MyList[a])
         a += 1
     else:
