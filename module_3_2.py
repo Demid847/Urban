7dae37f3684ff0dada0f1f25ed65bf0956b20b23
Demid847 <kriper847@gmail.com>
2024-08-18 11:45:37 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,20 @@
+def check_email(email):
+    if ("@" in email and email.endswith((".com", ".ru", ".net"))):
+        result = True
+    else:
+        result = False
+    return result
+def send_email (message, recipient, *,sender = "university.help@gmail.com"):
+    if not (check_email(recipient) and check_email(sender)):
+        print("Невозможно отправить письмо с адреса <sender> на адрес <recipient>")
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print("Письмо успешно отправлено с адреса <sender> на адрес <recipient>.")
+    else:
+        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> на адрес <recipient>.")
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
